d9a0accfee2c1c29164a48bb072ef3d4071bb755
Yilin Xu <yilin.xu@mastercard.com>
2016-08-22 13:17:13 -0400

    async

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -6,43 +6,18 @@ import * as courseActions from '../../actions/courseActions';
 class CoursesPage extends React.Component {
 	constructor(props, context) {
 		super(props, context);
-
-		this.state = {
-			course: { title: '' }
-		};
-
-		this.onTitleChange = this.onTitleChange.bind(this);
-		this.onClickSave = this.onClickSave.bind(this);
 	}
-
-	onTitleChange(event) {
-		const course = this.state.course; // assign this.state.course to course
-		course.title = event.target.value; // reassign course.title to whatever was in input
-		this.setState({course: course});  // reassign state course to new course object with updated title
-	}
-
-	onClickSave() {
-		this.props.actions.createCourse(this.state.course);
-	}
-
+	
 	courseRow(course, index) {
 		return <div key={index}>{course.title}</div>;
 	}
+	
 	render() {
 		return (
-				<div>
-					<h1>Courses</h1>
-					{this.props.courses.map(this.courseRow)}
-					<h2>Add Course</h2>
-					<input
-						type="text"
-						onChange={this.onTitleChange}
-						value={this.state.course.title} />
-					<input
-						type="submit"
-						value="Save"
-						onClick={this.onClickSave} />
-				</div>
+			<div>
+				<h1>Courses</h1>
+				{this.props.courses.map(this.courseRow)}
+			</div>
 		);
 	}
 }
