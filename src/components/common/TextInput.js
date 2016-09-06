12495523a76885a25ceb19bc4add6d046e0d22fb
Yilin Xu <yilin.xu@mastercard.com>
2016-09-06 13:42:52 -0400

    correct ampersand

--- src/components/common/TextInput.js
+++ src/components/common/TextInput.js
@@ -2,7 +2,7 @@ import React, {PropTypes} from 'react';
 
 const TextInput = ({name, label, onChange, placeholder, value, error}) => {
 	let wrapperClass = 'form-group';
-	if (error & error.length > 0) {
+	if (error && error.length > 0) {
 		wrapperClass += " " + 'has-error';
 	}
 	
